3f4ff8f905e2382175f3f711a5b4021c170753ac
Misha Pliskunov <pliskunov.mykhailo@gmail.com>
2017-12-18 21:16:16 +0200

    slick slider added

---
+++ js/main.js
@@ -0,0 +1,12 @@
+;(function($){
+	'use strict';
+
+	var teamSlider = $('.ba-team-slider');
+	teamSlider.slick({
+		dots: true,
+		slide: '.ba-slide',
+		prevArrow: '.ba-slider-prev',
+		nextArrow: '.ba-slider-next'
+	});
+
+})(jQuery);
